018969c4327597bde69430fb8a97e39491f9ea31
hazelofficial93 <108572880+hazelofficial93@users.noreply.github.com>
2022-07-13 02:13:00 +0700

    Add files via upload

---
+++ cybersecurity.py
@@ -0,0 +1,79 @@
+=============================
+# WELCOME TO CYBER SECURITY #
+# CREATOR BY : H4Z3L_OFFICIAL #
+# INSTAGRAM  : drrk_indo             #
+=============================
+
+echo "Silahkan masukkan name anda:" $name
+    read name
+  echo "--------------------"
+  echo " Nama:"
+  echo "--------------------"
+  echo "Silahkan pilih tools:" $pilih
+  
+   echo "1. install kali linux"
+   echo "2. bruteforce Wifi"
+   echo "3. bruteforce account Instagram"
+   echo "4. retas no whatsapp"
+   echo "5. bruteforce facebook "
+   echo "6. VSFTPD v2.3.4 backdoor command execution"
+   echo "7. exploiting wordpress website"
+   echo "8. bruteforce with metasploit"
+   echo "9. exploit cctv camera"
+   echo "10.payload generator"
+   echo "11.php reverse shell(Remote File Inclusion)
+   echo "12.scanning open ports"
+   echo "13.scan your network"
+   echo "14.scan TCP or UDP protocols"
+   echo "15.detect firewall settings"
+   echo "16.scan a firewall for mac address spoofing"
+   echo "17.default script scan"
+   echo "18.ip address information"
+   echo "19.scan list of hosts from a file"
+   echo "20.aggressive and obtrusive scan"
+   echo "21.Nmap -help
+   echo "22.OS detection"
+   echo "23.scan the most popular ports"
+   echo "24.exit"
+   
+    fi [ $pilih = "1" ]
+    fi [ $pilih = "1" ]
+  then          
+      echo " apt update -y "
+      echo " apt update -y "
+      echo " apt install wget -y "
+      echo " apt install curl -y "
+      echo " apt install ruby -y "
+      echo " apt install bash -y "
+      echo " pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh "
+      echo " ./start-kali.sh "   
+  fi 
+    if
+ fi [ $pilih = "2" ]
+ fi [ $pilih = "2" ]
+  then
+      echo " install wordlist terlebih dahulu "
+      echo " https://mega.nz/file/bToTlZzB "
+      echo " apt install update -y "          
+      echo " apt install upgrade -y "
+      echo " apt install git -y
+      echo " apt install wget -y
+      echo " apt install proot -y
+      echo " apt install curl -y
+      echo " pkg install clang -y
+      echo " pkg install tsudo -y
+      echo " pkg install tsu -y
+      echo " git clone https://github.com/kumpulanremaja/wifi "
+      echo " cd wifi "
+      echo " chmod +x wifi.sh "
+      echo " sh wifi.sh "
+  fi
+    if
+
+==========================
+WARNING! # ============== #
+THESE TOOLS REALLY WORK # ======
+SO I PLEASE USE THESE TOOLS WISE#===
+AND I PLEASE DON'T USE IT WRONG#=====
+THAT IS ALL AND THANK YOU 🙏#========
+=======================================
